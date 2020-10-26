7f11b0b99bad694302492453b1053a261d5ca33d
Jagoda Lewandowska <jadlewandowska@gmail.com>
2020-10-26 19:21:01 +0100

    Lab 1 - Podstawy WWW

--- Lab1/Assets/Scripts/main.js
+++ Lab1/Assets/Scripts/main.js
@@ -1,7 +1,73 @@
-console.log("Pierwszy skrypt!");
-for(let i=0; i<10; i++){
-    console.log("Liczba" + String(i));
+// Odnośniki
+
+function goBack(){
+    window.history.back();
 }
 
-document.getElementById("naglowek").style.backgroundColor="#4BA2EA";
-document.getElementsByTagName("p")[0].style.color="#F7F781";
+function goStronaGlowna(){
+  window.history.back("../index.html");
+}
+
+function zrodloListu(){
+    window.open("https://www.thebalancecareers.com/job-application-letter-sample-2062548","_blank");
+}
+
+function zrodloPodrecznika(){
+  window.open("https://encyklopedia.pwn.pl/haslo/calka;3882822.html#:~:text=ca%C5%82ka%2C,matematyki%2C%20wprowadzone%20niezale%C5%BCnie%20przez%20I.&text=ma%20szerokie%20zastosowania%20nie%20tylko,p%C3%B3l%20i%20obj%C4%99to%C5%9Bci%20figur%20geometrycznych).","_blank");
+}
+
+function github(){
+    window.open("https://github.com/jagodalewandowska","_blank");
+}
+
+// Formularz
+
+function wyslijOpinie() {
+    // Przekazywanie parametrów do wyświetlenia
+    var imie = document.getElementById("imie").value;
+    wyswietlImie.innerHTML= imie;
+    var opinia = document.getElementById("opinia").value;
+    wyswietlWiadomosc.innerHTML= opinia;
+
+    // Ukrywanie formularza
+    x = document.getElementById("ukryj");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+    // Wyświetlanie powiadomienia
+    y = document.getElementById("pokaz");
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+    }
+}
+
+// Wykres
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Rysowanie wykresu
+function drawChart() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Całki oznaczone', 20],
+    ['Całki nieoznaczone', 15],
+    ['Ćwiczenia', 50]
+  ]);
+
+  // Ustawienia wykresu
+  var options = {'title':'Podział godzin do nauki rozdziału',
+                 'width':240,
+                 'height':300,
+                 legend: 'bottom'};
+
+  // Przekazywanie parametrów
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
---
+++ Lab1/README.md
@@ -0,0 +1,37 @@
+# Lab 1
+##### Do wykonania strony wykorzystałam html, css, oraz js. Nie korzystałam jednak z szablonu Bootstrapa, jedynie z biblioteki **MathJax** oraz **Google Charts** do dodania wykresu na podstronie Podręcznik. Plik main.js zawiera funkcje, które przenoszą 
+
+####
+##### Wymagania dotyczące zadania pierwszego:
+
+![alt text](https://i.imgur.com/WaPDTHf.png)
+
+# Zrzuty ekranów poszczególnych stron
+
+### Strona główna
+##### Strona główna zawiera podstawowe informacje o autorze, a także odnośniki do poszczególnych stron: list, podręcznik oraz formularz. Znajdują się na niej przyciski w nawigacji oraz w stopce do mojego konta na GitHub.
+
+![alt text](https://i.imgur.com/kl8rpS6.png)
+
+### List
+##### Zawiera sformatowany list, a także odnośnik do źródła w którym znajduje się jego oryginalna treść, a także powrót do strony poprzedniej oraz głównej.
+
+![alt text](https://i.imgur.com/ezCp9eH.png)
+
+## Podręcznik
+##### Do utworzenia strony z podręcznika wykorzystałam: 
+- **MathJax** - do tworzenia wzorów matematycznych
+- **Google Charts** - do wykresu kołowego
+
+![alt text](https://i.imgur.com/zDo299W.png)
+
+## Formularz
+##### Przy formularzu wykorzystałam również funkcję w skrypcie, która przechowuje wpisanie wartości w polach imię oraz wpis, a następnie zwraca je, ukrywając w tym samym czasie formularz.
+
+![alt text](https://i.imgur.com/uJJYnvd.png)
+
+##### Po wysłaniu formularza:
+
+![alt text](https://i.imgur.com/SfNuhIO.png)
+
+
