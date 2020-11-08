b05a6a56081aa342f67c6b4d66669917ac0ed763
Jagoda Lewandowska <jadlewandowska@gmail.com>
2020-11-08 17:00:58 +0100

    Dzwieki i przeciaganie

--- Lab3/js/script.js
+++ Lab3/js/script.js
@@ -1,9 +1,8 @@
-/* Pieski */
-
-var img = document.getElementById("hug");
+/* ------------------------------- Pieski -------------------------------*/
 
 /* Najechanie na pieski */
 
+var img = document.getElementById("hug");
 img.addEventListener("mouseover", function() {
     img.src = "../imgs/after.png"; 
 })
@@ -13,9 +12,35 @@ img.addEventListener("mouseout", function() {
 
 /* Kliknięcie na pieski */
 
+var ile = 0;
 img.addEventListener("click", function szczeka() {
     document.getElementById("szczek").innerHTML += "Szczek!<br>"
+    ile++;
+    if  (ile == 10) {
+        alert("Wystarczy szczekania!");
+        wyczysc();
+        ile = 0;        
+    }
 })
+
+/* Zagraj dźwięk przy kliknięciu */
+
+document.addEventListener("keydown", mial);
+
+function mial(e) {
+	if (e.keyCode === 77) {        
+        document.getElementById("meow").play();
+        document.getElementById("kotek").style="display:block";
+	}
+}
+
+/* Kombinacja Ctrl + C */
+
+document.addEventListener("keydown", function(event) {
+    if (event.ctrlKey && event.key === 'c') {
+      alert("Skopiowane!");
+    }
+});
 
 /* Wyczyszczenie okna */
 
@@ -23,6 +48,17 @@ function wyczysc() {
     document.getElementById("szczek").innerHTML = "";
 }
 
+/* Przeciągnij kość */
+
+function drop(ev) {
+    // Zapobiegaj normalnej reakcji przeglądarki -- kopiowanie, przeciąganie
+    ev.preventDefault();
+  }
+
+document.addEventListener("drop", function () {
+    document.getElementById("miska").src = "../imgs/bowlbone.png";
+    document.getElementById("kosc").style = "display:none";
+})  
 
 console.info("Działam");
 
