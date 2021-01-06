b0b5b4f3c6e088d5803bc326af8a37cda33d9158
Jagoda Lewandowska <jadlewandowska@gmail.com>
2021-01-06 19:20:35 +0100

    for git diff usage 3

--- Lab8/src/Chart.jsx
+++ Lab8/src/Chart.jsx
@@ -5,19 +5,9 @@ import ReactScoreIndicator from 'react-score-indicator'
 class Chart extends Component {
   render () {
     return (
-      <ReactScoreIndicator
-        /* Wartość */
-        value={45}
-        /* Max wartość */
+      <ReactStoreIndicator
+        value={30}
         maxValue={100}
-        /* Szerokość */
-        width={200}
-        /* Grubość linii */
-        lineWidth={20}
-        /* Odległość pomiędzy */
-        lineGap={10}
-        /* Przezroczystość elementów ponad wartość */
-        fadedOpacity={20}    
       />
     )
   }
