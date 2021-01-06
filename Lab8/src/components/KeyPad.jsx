daede2115c461259479675980a373907d8849cf2
Jagoda Lewandowska <jadlewandowska@gmail.com>
2021-01-06 19:12:34 +0100

    for git diff usage p2

--- Lab8/src/components/KeyPad.jsx
+++ Lab8/src/components/KeyPad.jsx
@@ -5,33 +5,35 @@ class KeyPad extends Component {
         return (
             // Przy każdym kliknięciu wywoływany będzie this.props.OnClick który przekazuje parametr
             <div className="button">
-                <button name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
-                <button name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
-                <button name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
-                <button name="C" onClick={e => this.props.onClick(e.target.name)}>C</button><br/>
+                {/* Pierwszy rząd*/}
+                <button class="btn btn-outline-info" name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
+                <button class="btn btn-outline-info" name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
+                <button class="btn btn-outline-info" name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
+                <button class="btn btn-outline-info" name="C" onClick={e => this.props.onClick(e.target.name)}>C</button><br/>
+                
+                {/* Drugi rząd*/}
+                <button class="btn btn-outline-info" name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
+                <button class="btn btn-outline-info" name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
+                <button class="btn btn-outline-info" name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
+                <button class="btn btn-outline-info" name="+" onClick={e => this.props.onClick(e.target.name)}>+</button><br/>
 
+                {/* Trzeci rząd*/}
+                <button class="btn btn-outline-info" name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
+                <button class="btn btn-outline-info" name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
+                <button class="btn btn-outline-info" name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
+                <button class="btn btn-outline-info" name="-" onClick={e => this.props.onClick(e.target.name)}>-</button><br/>
 
-                <button name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
-                <button name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
-                <button name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
-                <button name="+" onClick={e => this.props.onClick(e.target.name)}>+</button><br/>
+                {/* Czwarty rząd*/}
+                <button class="btn btn-outline-info" name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
+                <button class="btn btn-outline-info" name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
+                <button class="btn btn-outline-info" name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
+                <button class="btn btn-outline-info" name="*" onClick={e => this.props.onClick(e.target.name)}>x</button><br/>
 
-
-                <button name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
-                <button name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
-                <button name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
-                <button name="-" onClick={e => this.props.onClick(e.target.name)}>-</button><br/>
-
-                <button name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
-                <button name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
-                <button name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
-                <button name="*" onClick={e => this.props.onClick(e.target.name)}>x</button><br/>
-
-
-                <button name="." onClick={e => this.props.onClick(e.target.name)}>.</button>
-                <button name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
-                <button name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
-                <button name="/" onClick={e => this.props.onClick(e.target.name)}>÷</button><br/>
+                {/* Piąty rząd*/}
+                <button class="btn btn-outline-info" name="." onClick={e => this.props.onClick(e.target.name)}>.</button>
+                <button class="btn btn-outline-info" name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
+                <button class="btn btn-outline-info" name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
+                <button class="btn btn-outline-info" name="/" onClick={e => this.props.onClick(e.target.name)}>÷</button><br/>
             </div>
         );
     }
