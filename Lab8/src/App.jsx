100d5e84d245523bccb0ea01816bbe5e29266dd0
Jagoda Lewandowska <jadlewandowska@gmail.com>
2021-01-06 14:04:32 +0100

    just started + first comp

---
+++ Lab8/src/App.jsx
@@ -0,0 +1,51 @@
+import React, {Component} from "react";
+import './App.css';
+import Home from './Home'
+import Typical from 'react-typical'
+import wym from './images/wymagania.png';
+
+import {
+  /* główny komponent */
+  BrowserRouter as Router,
+  /* Renderowanie pierwszej lokalizacji która pasuje - trasa */
+  Switch,
+  /* Definiowanie ścieżek */
+  Route,
+  /* Przełączanie między ścieżkami */
+  Link
+} from "react-router-dom";
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div>
+        <nav className="navbar navbar-expand-sm navbar-dark">
+          <div className="navbar-nav">
+            <Link to="/" className="nav-link active">Homepage</Link>
+            <Link to="/app" className="nav-link">App</Link>
+          </div>
+        </nav>
+        <Switch>
+          <Route path="/app">
+          </Route>
+          <Route path="/">
+            <div className="typical">         
+              <Typical
+                steps={['Laboratorium 8', 1000, 
+                'Laboratorium 8 - Analiza kodu źródłowego', 200,
+                'Wykorzystanie git difftool', 800]}
+                loop={Infinity}
+              />            
+            </div>  
+            <Home />      
+            <img src={wym} alt="wymagania"/>    
+          </Route>
+        </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ Lab8/src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => (   
+    <div class="card">
+        <div class="card-header">
+            Wymagania: 
+        </div>    
+    </div>    
+);
+
+export default Home;
